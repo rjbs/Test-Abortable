801de3b5e4dbe1a5260146b5cf5e54402df99238
Ricardo Signes <rjbs@cpan.org>
2016-11-30 11:10:05 +1100

    first go, pulled out of first go in Test-Routine

---
+++ lib/Test/Abortable.pm
@@ -0,0 +1,40 @@
+use strict;
+use warnings;
+package Test::Abortable;
+# ABSTRACT: subtests that you can die your way out of ... but still live
+
+use Test2::API 1.302045 ();
+
+sub subtest {
+  my ($self, $name, $code) = @_;
+
+  my $ctx = Test2::API::context();
+
+  my $pass = Test2::API::run_subtest($name, sub {
+    my $ctx = Test2::API::context();
+
+    my $ok = eval { $self->$name; 1 };
+
+    if (! $ok) {
+      my $error = $@;
+      if (ref $error and my $events = eval { $error->as_test_abort_events }) {
+        for (@$events) {
+          my $e = $ctx->send_event(@$_);
+          $e->set_meta(test_abort_object => $error)
+        }
+      } else {
+        die $error;
+      }
+    }
+
+    $ctx->release;
+
+    return;
+  });
+
+  $ctx->release;
+
+  return $pass;
+}
+
+1;
